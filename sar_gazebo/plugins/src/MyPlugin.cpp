254d5dbc0a90e273f757c88e3f5e9bb64f630b72
DLee0307 <thakd037@gmail.com>
2024-02-06 16:32:50 -0500

    MyPluigin have to contain in world part not gui

--- sar_gazebo/plugins/src/MyPlugin.cpp
+++ sar_gazebo/plugins/src/MyPlugin.cpp
@@ -1,19 +1,27 @@
 #include "MyPlugin.h"
+#include <iostream>
 
 void MyPlugin::Configure(const ignition::gazebo::Entity &_entity,
                          const std::shared_ptr<const sdf::Element> &_sdf,
                          ignition::gazebo::EntityComponentManager &_ecm,
                          ignition::gazebo::EventManager &_eventMgr)
-
 {
-    std::cout << "MyPlugin::Configure called for entity: " << _entity << std::endl;
+    std::cout << "Configuring MyPlugin for entity: " << _entity << std::endl;
+
+    // Example: Read a parameter from the SDF file
+    if (_sdf->HasElement("example_param")) {
+        auto exampleParam = _sdf->Get<std::string>("example_param");
+        std::cout << "Example parameter: " << exampleParam << std::endl;
+    } else {
+        std::cout << "No example_param found in SDF." << std::endl;
+    }
 }
 
 void MyPlugin::PreUpdate(const ignition::gazebo::UpdateInfo &_info,
                          ignition::gazebo::EntityComponentManager &_ecm)
-
 {
-    // For simplification, no operation is performed here
+    std::cout << "PreUpdate called at simulation time: " 
+              << _info.simTime.count() << std::endl;
 }
 
 IGNITION_ADD_PLUGIN(MyPlugin,
