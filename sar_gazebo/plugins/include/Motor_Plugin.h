b7cc89282e0dd3b9e7390c8af5d301019bb91b88
DLee0307 <thakd037@gmail.com>
2024-02-07 17:23:02 -0500

    get the joint name

--- sar_gazebo/plugins/include/Motor_Plugin.h
+++ sar_gazebo/plugins/include/Motor_Plugin.h
@@ -10,6 +10,16 @@
 //https://gazebosim.org/api/gazebo/5.0/src_2systems_2physics_2Physics_8hh.html
 #include <ignition/transport/Node.hh>
 #include <ignition/common/Time.hh>
+#include <sdf/sdf.hh>
+
+// CUSTOM INCLUDES
+//#include "sar_msgs/CTRL_Data.h"
+//#include "sar_msgs/MS.h"
+//#include <sar_msgs/msg/ctrl_data.hpp>
+//#include <sar_msgs/msg/ms.hpp>
+
+#define g2Newton (9.81f/1000.0f)
+#define Newton2g (1000.0f/9.81f)
 
 class Motor_Plugin : public ignition::gazebo::System,
                      public ignition::gazebo::ISystemConfigure,
@@ -17,7 +27,12 @@ class Motor_Plugin : public ignition::gazebo::System,
 {
 public:
     // Constructor
-    Motor_Plugin() = default;
+    Motor_Plugin(){
+        // make node
+        rclcpp::init(0, nullptr);
+        node_ = rclcpp::Node::make_shared("motor_plugin_node");            
+            
+    }
 
     // Destructor
     virtual ~Motor_Plugin() = default;
@@ -32,5 +47,56 @@ public:
     void PreUpdate(const ignition::gazebo::UpdateInfo &_info,
                    ignition::gazebo::EntityComponentManager &_ecm) override;
 private:
-    bool messagePrinted = false;
+            // need to check
+            ignition::gazebo::Model model;
+            
+            //DH's editing make node
+            rclcpp::Node::SharedPtr node_;
+            
+            
+            // CONFIG PARAMS
+            std::string SAR_Type;
+            std::string SAR_Config;
+            std::string Cam_Config;
+
+            // GAZEBO POINTERS
+            ignition::gazebo::Entity worldEntity;    // physics::WorldPtr
+            ignition::gazebo::Entity modelEntity;    // physics::ModelPtr
+            ignition::gazebo::Entity jointEntity;    // physics::JointPtr
+            ignition::gazebo::Entity linkEntity;     // physics::LinkPtr
+
+            // MOTOR PARAMETERS
+            std::string Motor_Joint_Name;
+            std::string Prop_Link_Name;
+
+            int Motor_Number;
+            int Turn_Direction;
+
+            double Thrust_Coeff;    // Thrust Coeff [N/rad/s]
+            double Torque_Coeff;    // Torque Coeff [N*m/rad/s]
+            double C_tf;        // Torque-Thrust Coeff [N*m/N]
+
+            // FIRST ORDER FILTER BEHAVIOR
+            float Thrust_input = 0.0f;  // Desired Thrust [N]
+            double Tau_up;              // Motor Time Constant (Up) [s]
+            double Tau_down;            // Motor Time Constant (Down) [s]
+            double Sampling_time;
+            double Prev_Sim_time = 0.0;
+            double Prev_Thrust = 0.0;
+            
+            // CACULATED VALUES
+            double Thrust;              // Calculated Thrust [N]
+            double Torque;              // Calculated Torque [N*m]
+            double Rot_Vel = 0.0f;      // Rotational Velocity [rad/s]
+            double Rot_Vel_Slowdown;    // Slowed-down Rotational Velocity [rad/s]
+    
+
+            // ROS CONNECTIONS
+            
+            //ros::NodeHandle nh;
+            //make node using 
+
+            //ros::Subscriber CTRL_Data_Sub = nh.subscribe<sar_msgs::CTRL_Data>("/CTRL/data", 1, &Motor_Plugin::CtrlData_Callback, this, ros::TransportHints().tcpNoDelay());
+            //ros::Publisher MS_Data_Pub = nh.advertise<sar_msgs::MS>("/SAR_Internal/MS",1);
+            //sar_msgs::MS Thrust_msg;
 };
--- sar_gazebo/plugins/src/Motor_Plugin.cpp
+++ sar_gazebo/plugins/src/Motor_Plugin.cpp
@@ -6,13 +6,31 @@ void Motor_Plugin::Configure(const ignition::gazebo::Entity &_entity,
                          ignition::gazebo::EntityComponentManager &_ecm,
                          ignition::gazebo::EventManager &_eventMgr)
 {
-    std::cout << "Loading Motor_Plugin!!!!!!\n";
+    // Check plugin is running
+    std::cout << "Loading Motor_Plugin!!!\n";
+    
+    // need to check
+    this->model = ignition::gazebo::Model(_entity);
 
+    if (!_sdf->HasElement("Joint_Name")){
+        ignerr << "Missing required parameter <Joint_Name>" << std::endl;
+        return;}
+
+    std::string Motor_Joint_Name = _sdf->Get<std::string>("Joint_Name");
+    std::cout << "\t Joint Name:\t" << Motor_Joint_Name << std::endl;
+
+    // Find the joint entity by name
+    auto jointEntity = this->model.JointByName(_ecm, Motor_Joint_Name);
+    if (jointEntity == ignition::gazebo::kNullEntity){
+        ignerr << "Joint '" << Motor_Joint_Name << "' not found." << std::endl;
+        return;}
 }
+
 void Motor_Plugin::PreUpdate(const ignition::gazebo::UpdateInfo &_info,
                          ignition::gazebo::EntityComponentManager &_ecm)
 {
-
+    //(void)_info; // Explicitly mark _info as unused
+    //(void)_ecm;  // Explicitly mark _ecm as unused
 }
 
 IGNITION_ADD_PLUGIN(Motor_Plugin,
